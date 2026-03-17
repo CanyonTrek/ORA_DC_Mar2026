e2841ed31264d318bd219724433a9db7bdd37367
Donald Cameron <donald.cameron@qa.com>
2026-03-17 18:45:38 +0000

    Created demo_iterator_for_loop.py script

---
+++ demo_iterator_for_loop.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+# Author: DCameron
+# Description: This script will demo HOWTO ITERATOR through
+# a SEQUENCE (str/tuple/list/dict/sets) using an ITERATOR for
+# loop
+""" 
+    DocString
+"""
+#               0          1         2       3         4        5
+students = ['brianna', 'sharath', 'john', 'savita', 'celso', 'sofia']
+
+# ITERATE through the student using an
+# ITERATOR for loop
+for name in students:
+    print(name, end="\n")
+print("Students: ", students)
+
+# ITERATE through the students and modify the names using an
+# ITERATOR for loop
+idx = 0
+for name in students:
+    print(name.upper(), end="\n")
+    students[idx] = name.upper()
+    idx += 1
+print("Students: ", students)
+
+# ITERATE through the students and modify the names using an
+# ITERATOR for loop plus the built-in enumerate() function
+for (idx, name) in enumerate(students, start=0):
+    print(name.title(), end="\n")
+    students[idx] = name.title()
+print("Students: ", students)
