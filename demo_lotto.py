3c2e865e70cfc6c9c94ee7bd83e2dcf473c2f69f
Donald Cameron <donald.cameron@qa.com>
2026-03-18 15:55:42 +0000

    Updated demo_lotto.py to use sets

--- demo_lotto.py
+++ demo_lotto.py
@@ -7,15 +7,21 @@
 """
 import random
 
-lotto = [] # Create empty list to store lottery numbers
+# lotto = [] # Create empty list to store lottery numbers
 
 # Repeat whilst condition remains True
+# while len(lotto) < 6:
+#    num = random.randint(1, 50)
+#    if num not in lotto:
+#        lotto.append(num)
+#    else:
+#        print("Duplicate number:", num)
+
+lotto = set()
+
+# A Pythonic solution!
 while len(lotto) < 6:
     num = random.randint(1, 50)
-    if num not in lotto:
-        lotto.append(num)
-    else:
-        print("Duplicate number:", num)
+    lotto.add(num)
 
-
-print("lottery numbers =", lotto)
+print("lottery numbers =", sorted(lotto))
