13d65a3db70bb1d991a99c557f9a2da02d2c5bb3
Donald Cameron <donald.cameron@qa.com>
2026-03-19 23:40:30 +0000

    Updated demo_user_variadic_function.py

--- demo_user_variadic_function.py
+++ demo_user_variadic_function.py
@@ -8,6 +8,7 @@
     Regex patterns in one or more file/s
 """
 import re
+import sys
 
 # Example of a VARIADIC function which unpacks
 # all remaining parameters into a TUPLE
@@ -17,16 +18,36 @@ def search_pattern(pattern=r"^.{19}$", *files):
     """
     lines = 0
     for file in files:
-        fh_words = open(file, mode="rt")
+        try:
+            fh_words = open(file, mode="rt")
+        except FileNotFoundError as err:
+            print(f"Error code {err.args[0]}, msg={err.args[1]}, {err.filename}", file=sys.stderr)
+            sys.exit(1)
+        except PermissionError as err:
+            print(f"Error, {err.args}, {err.filename}", file=sys.stderr)
+            sys.exit(2)
+        except Exception as err:
+            print("Some other error occurred. Investigate", file=sys.stderr)
+            sys.exit(3)
+        else:
+            # Execute if try block succeeds
+            for line in fh_words:
+                m = re.search(pattern, line)  # Match pattern
+                if m:
+                    lines += 1
+                    print(line, end="")
+            fh_words.close() # Close file handle
+        finally:
+            # Always executes
+            print("And now for something completely different")
 
-        for line in fh_words:
-            m = re.search(pattern, line)  # Match pattern
-            if m:
-                lines += 1
-                print(line, end="")
-
-        fh_words.close() # Close file handle
     return lines
 
-num_lines = search_pattern(r"^([A-Z]).*\1$", r"f:\labs\words", r"f:\labs\words2", r"F:\labs\words3")
-print(f"Matched {num_lines} lines")
+def main():
+    num_lines = search_pattern(r"^([A-Z]).*\1$", r"f:\labs\words", r"f:\labs\words2", r"F:\labs\words3")
+    print(f"Matched {num_lines} lines")
+    return None
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
