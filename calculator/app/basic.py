0791d596fde2a2f65b177e5938aa327952a61293
Donald Cameron <donald.cameron@qa.com>
2026-03-20 00:12:48 +0000

    Updated basic.py with doctests

--- calculator/app/basic.py
+++ calculator/app/basic.py
@@ -4,24 +4,36 @@
 # for a calculator app
 """ 
     Calc App with add, multiply and divide functions
+    Used by >>> help()
 """
 import sys
 def add(*args):
-    """ Return SUM of all parameters as a float """
+    """ Return SUM of all parameters as a float
+    >>> add(4, 3, 2, 1)
+    10.0
+    >>> add(30, 20)
+    50.0
+    """
     sum = 0
     for num in args:
         sum += num
     return float(sum)
 
 def mul(*args):
-    """ Return PRODUCT of all parameters as a float """
+    """ Return PRODUCT of all parameters as a float
+    >>> mul(4, 3, 2)
+    24.0
+    """
     total = 1
     for num in args:
         total *= num
     return float(total)
 
 def div(x, z):
-    """ Return QUOTIENT of x divided by z to 3 decimal places """
+    """ Return QUOTIENT of x divided by z to 3 decimal places
+    >>> div(4, 3)
+    1.333
+    """
     return round(x/z, 3)
 
 def main():
@@ -36,5 +48,7 @@ def main():
 if __name__ == "__main__":
     # Execute if RAN DIRECTLY as a program
     # Ignored if imported as a module
+    import doctest
+    doctest.testmod()
     main()
     sys.exit(0) # Explicit EXIT with error code
